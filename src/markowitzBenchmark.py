dc55d167730857834f6577c70a3c1362063d6bd1
Pereira de Almeida Hugo Gabriel <hugo@MacBook-Pro-de-Pereira.local>
2025-11-22 12:42:58 +0100

    creation of markowitz function

--- src/markowitzBenchmark.py
+++ src/markowitzBenchmark.py
@@ -6,25 +6,24 @@ import os
 CLOSE_PRISES_PATH = os.path.join('datasets','marketData','adj_close_prices.csv')
 WEIGHTS_PATH = os.path.join('datasets','groundTruth')
 
-print(os.getcwd())
-print(CLOSE_PRISES_PATH)
-print(WEIGHTS_PATH)
+def markowitzBenchmatk():
+    dataframe = pd.read_csv(CLOSE_PRISES_PATH, sep = ",")
 
-dataframe = pd.read_csv(CLOSE_PRISES_PATH, sep = ",")
+    date_column_name = dataframe.columns[0] 
+    prices_df = dataframe.set_index(date_column_name)
 
-date_column_name = dataframe.columns[0] 
-prices_df = dataframe.set_index(date_column_name)
+    mu = pypo.expected_returns.mean_historical_return(prices_df)
+    S = pypo.risk_models.sample_cov(prices_df)
 
-mu = pypo.expected_returns.mean_historical_return(prices_df)
-S = pypo.risk_models.sample_cov(prices_df)
+    ef = pypo.EfficientFrontier(mu, S)
+    weights = ef.max_sharpe()
+    expected_return, volatility, sharpe = ef.portfolio_performance(verbose=True)
 
-ef = pypo.EfficientFrontier(mu, S)
-weights = ef.max_sharpe()
-expected_return, volatility, sharpe = ef.portfolio_performance(verbose=True)
+    weights_df = pd.DataFrame.from_dict(weights, orient='index', columns=['Weights'])
 
-weights_df = pd.DataFrame.from_dict(weights, orient='index', columns=['Weights'])
+    weights_df.to_csv(os.path.join(WEIGHTS_PATH,'weights.csv'))
 
-weights_df.to_csv(os.path.join(WEIGHTS_PATH,'weights.csv'))
+    return expected_return, volatility, sharpe
 
-print(sum(weights_df.values))
-print("\n", weights_df)
+
+markowitzBenchmatk()
