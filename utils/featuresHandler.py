abfeb8cd82b35e7c9a97be4c40acefdaeaf31ea9
Hugo Gabriel Pereira de Almeida <hugogpdealmeida@gmail.com>
2026-01-06 11:43:51 +0100

    Add new features file -- Add volatility features and normalized returns

---
+++ utils/featuresHandler.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import os
+
+lambda_short = 0.94   
+lambda_long = 0.97    
+annualization = np.sqrt(252)
+
+def ensure_clean_dirs():
+    """Create expected cleaned subfolders if they don't exist."""
+    dirs = [
+        "data/features/",
+        "data/features/volatility",
+        "data/features/volatility/",
+        "data/features/returns/"
+    ]
+    for d in dirs:
+        os.makedirs(d, exist_ok=True)
+
+def ewma(df,decay):
+    
+    
+    vol = pd.DataFrame(index=df.index, columns=df.columns, dtype=float)
+
+    vol.iloc[0] = df.iloc[:20].var() ** 0.5
+
+    for t in range(1, len(df)):
+        vol.iloc[t] = np.sqrt(
+            decay * (vol.iloc[t-1] ** 2) + (1 - decay) * (df.iloc[t-1] ** 2)
+        )
+
+    return vol * annualization
+
+def vol_zscore(short_vol,decay):
+    mu = ewma(df=short_vol,decay=decay)
+    sigma = ewma(df=(short_vol-mu)**2,decay=decay)
+
+    zscore = (short_vol - mu) / sigma
+
+    return zscore
+
+if __name__ == '__main__':
+    ensure_clean_dirs()
+
+    returns = pd.read_csv("data/cleaned/returns/returns.csv", parse_dates=['Date'], index_col='Date')
+
+    short_vol = ewma(df=returns,decay= lambda_short)
+    long_vol = ewma(df=returns, decay= lambda_long)
+    
+    vol_ratio = short_vol / long_vol
+
+    norm_return = returns / short_vol
+
+    zscore = vol_zscore(short_vol,0.97)
+
+    short_vol.to_csv("data/features/volatility/short_ratio.csv")
+    long_vol.to_csv("data/features/volatility/long_ratio.csv")
+    vol_ratio.to_csv("data/features/volatility/vol_ratio.csv")
+    zscore.to_csv("data/features/volatility/zscore.csv")
+    norm_return.to_csv("data/features/returns/norm_returns.csv")
+   
