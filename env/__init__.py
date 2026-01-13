6f3c2b6ae65d2e631cec182966ddd8643317b030
Hugo Gabriel Pereira de Almeida <hugogpdealmeida@gmail.com>
2026-01-13 14:11:38 +0100

    Start env and add __init__.py

---
+++ env/__init__.py
@@ -0,0 +1,4 @@
+from .optiFolioEnv import OptiFolioEnv
+
+__version__ = ""
+__all__ = [OptiFolioEnv]
--- env/optiFolioEnv.py
+++ env/optiFolioEnv.py
@@ -1,8 +1,20 @@
 import gymnasium as gym
 
-class optiFolioEnv(gym.Env):
+import pandas as pd 
+
+class OptiFolioEnv(gym.Env):
     """
     Custom Gymnasium environment for portfolio optimization.
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, dataframe : pd.DataFrame, initial_amount = 1_000):
+        super(OptiFolioEnv).__init__()
+        self.action_space = gym.spaces.Discrete() 
+
+
+    def reset(self, *, seed = None, options = None):
+        pass
+
+    def step(self,action):
+        pass
+
+        
